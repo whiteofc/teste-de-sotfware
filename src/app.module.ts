54006d95ada8bb19110156dbc415c337e1192762
whiteofc <whitemusicofc@gmail.com>
2023-11-06 20:08:09 -0300

    fisrt commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { MessageModule } from './message/message.module';
+
+@Module({
+  imports: [UserModule, TaskModule, MessageModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
